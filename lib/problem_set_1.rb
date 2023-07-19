7d676787701aea21292baaa08ee526fe98174019
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-19 12:10:21 -0600

    write method for palindrome check

--- lib/problem_set_1.rb
+++ lib/problem_set_1.rb
@@ -4,7 +4,11 @@ def anagram?(word1, word2)
   word1.chars.sort == word2.chars.sort
 end
 
-##Create a method 
+##Create a method that checks if a word is a palindrome
+
+def palindrome?(word)
+  word.reverse == word
+end
 
 
 
