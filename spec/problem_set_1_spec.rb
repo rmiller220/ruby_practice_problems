5776304a67f9e0bb1523b68ee5c6c68cfcee676c
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-19 12:10:03 -0600

    write tests for palidrome, add test for anagram

--- spec/problem_set_1_spec.rb
+++ spec/problem_set_1_spec.rb
@@ -1,9 +1,23 @@
 require 'problem_set_1'
 
 RSpec.describe 'Problem Set 1' do
-  describe 'problem 1' do
+  describe 'Anagram' do
     it 'returns true if words are anagrams' do
       expect(anagram?('cinema', 'iceman')).to eq(true)
+    end
+
+    it 'returns false if words are not anagrams' do
+      expect(anagram?('cinema', 'icemans')).to eq(false)
+    end
+  end
+
+  describe 'Palindrome' do
+    it 'returns true if word is a palindrome' do
+      expect(palindrome?('racecar')).to eq(true)
+    end
+
+    it 'returns false if word is not a palindrome' do
+      expect(palindrome?('racecars')).to eq(false)
     end
   end
 end
