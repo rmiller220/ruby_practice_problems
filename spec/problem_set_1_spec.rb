e2ccca5bee196a6e61e0fcd6cbfb72e61557a3c3
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-20 16:55:47 -0600

    Write tests for two sum problem set 1

--- spec/problem_set_1_spec.rb
+++ spec/problem_set_1_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe 'Problem Set 1' do
       expect(palindrome?('racecars')).to eq(false)
     end
   end
+
+  describe 'Two Sum' do
+    it 'returns the indices of the two numbers that add up to the target' do
+      expect(two_sum([2, 7, 11, 15], 9)).to eq([0, 1])
+      expect(two_sum([3, 2, 4], 6)).to eq([1, 2])
+      expect(two_sum([3, 3], 6)).to eq([0, 1])
+    end
+  end
 end
