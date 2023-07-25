a47e559ec9ffa7843ff140c804184006a3426467
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-25 12:09:04 -0600

    add contains duplicate method

--- lib/problem_set_1.rb
+++ lib/problem_set_1.rb
@@ -25,6 +25,13 @@ def two_sum(nums, target)
  end
 end
 
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+def contains_duplicate(nums)
+  nums.uniq.length != nums.length
+end
+
+
 # For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 # Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 # I can be placed before V (5) and X (10) to make 4 and 9. 
@@ -33,5 +40,5 @@ end
 # Given a roman numeral, convert it to an integer.
 
 def roman_to_int(s)
-  
+
 end
