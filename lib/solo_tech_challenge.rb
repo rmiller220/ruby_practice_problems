37fd873f71866a4938fbc6e837be1281d74d2ad4
Reilly Miller <reillyjmiller31@gmail.com>
2023-07-31 15:35:51 -0600

    new problem, interview practice

--- lib/solo_tech_challenge.rb
+++ lib/solo_tech_challenge.rb
@@ -108,3 +108,33 @@ end
   #   end
   # end
 # end
+
+# A palindrome is any number, word, or phrase that reads the same forward as it does backward. In this challenge, 
+# we are going to focus on palindromic numbers. For example, 12321 is a palindromic number, whereas 123 is not.
+# Your goal is to write a method/function that takes in an integer and returns the next palindrome. 
+# It is safe to assume you are working with only whole numbers, no decimals, and no negatives.
+
+  # #Ruby
+  # find_next_palindrome(100)
+  # => 101
+
+  # find_next_palindrome(101)
+  # => 111
+
+  #num add 1 to it 
+  #set a variable = to number + 1
+  #while or until loop?
+  #put into a string 
+  #check if string and reverse string are the same, if so stop and return that number
+
+  def find_next_palindrome(num)
+    number = num += 1
+    until number.to_s == number.to_s.reverse
+      number = number += 1
+    end
+    number
+  end
+
+  num1 = 101
+
+print find_next_palindrome(num1)
