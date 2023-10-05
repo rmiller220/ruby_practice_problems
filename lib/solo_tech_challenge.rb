6f7281b7227a461493e5048687929f6389b33ec6
Reilly Miller <reillyjmiller31@gmail.com>
2023-10-05 14:10:32 -0600

    testing output

--- lib/solo_tech_challenge.rb
+++ lib/solo_tech_challenge.rb
@@ -139,6 +139,7 @@ end
 
 print find_next_palindrome(num1)
 
+puts ([0] + [0]).length
 
 # You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 # Merge nums1 and nums2 into a single array sorted in non-decreasing order.
