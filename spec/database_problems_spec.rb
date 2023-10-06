bb35510fdac3943ad641092923a26ef21741fffc
Reilly Miller <reillyjmiller31@gmail.com>
2023-10-06 10:41:33 -0600

    delete spec file from db probs

--- spec/database_problems_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'database_problems'
-
-RSpec.describe 'Database Problems' do
-  describe 'combining two tables' do
-    it 'returns name of person and city' do
-      expect(combining_two_tables).to eq([["Allen", "Wang", nil, nil], ["Bob", "Alice", "New York City", "New York"]])
-    end
-  end
-end
